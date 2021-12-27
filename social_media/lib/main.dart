ee1b96c9d77af0af79b04c9185e89dda2656ea67
alihan-ulker <ali.ulker.217@gmail.com>
2021-12-27 23:37:54 +0300

    Update main.dart

--- social_media/lib/main.dart
+++ social_media/lib/main.dart
@@ -1,8 +1,13 @@
 //@dart=2.9
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:social_media/routing.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
+
   runApp(const MyApp());
 }
 
