12b34ea0face4a4f75887e1e0b718568c627c6a6
alihan-ulker <ali.ulker.217@gmail.com>
2021-12-28 17:09:11 +0300

    login_pade.dart updated
    Form and Validation  controls were provided.

--- social_media/lib/pages/login_page.dart
+++ social_media/lib/pages/login_page.dart
@@ -8,89 +8,138 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        padding: const EdgeInsets.only(
-          left: 20.0,
-          right: 20.0,
-          top: 60.0,
+      body: Form(
+        key: _formKey,
+        child: ListView(
+          padding: const EdgeInsets.only(
+            left: 20.0,
+            right: 20.0,
+            top: 60.0,
+          ),
+          children: [
+            const FlutterLogo(
+              size: 90.0,
+            ),
+            const SizedBox(
+              height: 80.0,
+            ),
+            TextFormField(
+              //Keyboard autocomplete.
+              autocorrect: true,
+              //Keyboard type is set to email.
+              keyboardType: TextInputType.emailAddress,
+              decoration: const InputDecoration(
+                hintText: "Email adresinizi giriniz.",
+                //Error message style
+                errorStyle: TextStyle(fontSize: 16.0),
+                //Adding an icon to the head.
+                prefixIcon: Icon(Icons.mail),
+              ),
+
+              //Entered value check
+              validator: (inputValue) {
+                if (inputValue!.isEmpty) {
+                  return "Email alanı boş bırakılamaz!";
+                } else if (!inputValue.contains("@")) {
+                  return "Girilen değer mail formatında olmalıdır!";
+                }
+                return null;
+              },
+            ),
+            const SizedBox(
+              height: 40.0,
+            ),
+            TextFormField(
+              //To hide the password.
+              obscureText: true,
+              decoration: const InputDecoration(
+                hintText: "Şifrenizi giriniz.",
+                //Error message style
+                errorStyle: TextStyle(fontSize: 16.0),
+                //Adding an icon to the head.
+                prefixIcon: Icon(Icons.lock),
+              ),
+
+              //Entered value check
+              validator: (inputValue) {
+                if (inputValue!.isEmpty) {
+                  return "Şifre alanı boş bırakılamaz!";
+                } else if (inputValue.trim().length < 4) {
+                  return "Şifre 4 karakterden daha az olamaz!";
+                }
+                return null;
+              },
+            ),
+            const SizedBox(
+              height: 40.0,
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: TextButton(
+                    onPressed: () {},
+                    style: TextButton.styleFrom(
+                      primary: Colors.white,
+                      backgroundColor: Colors.blue,
+                    ),
+                    child: const Text(
+                      "Hesap Oluştur",
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  width: 20.0,
+                ),
+                Expanded(
+                  child: TextButton(
+                    onPressed: _login,
+                    style: TextButton.styleFrom(
+                      primary: Colors.white,
+                      backgroundColor: Colors.blue,
+                    ),
+                    child: const Text(
+                      "Giriş Yap",
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20.0),
+            const Center(child: Text("veya")),
+            const SizedBox(height: 20.0),
+            const Center(
+                child: Text(
+              "Google ile Giriş Yap",
+              style: TextStyle(
+                fontSize: 19.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.grey,
+              ),
+            )),
+            const SizedBox(height: 20.0),
+            const Center(child: Text("Şifremi Unuttum")),
+          ],
         ),
-        children: [
-          const FlutterLogo(
-            size: 90.0,
-          ),
-          const SizedBox(
-            height: 80.0,
-          ),
-          TextFormField(
-            //Keyboard autocomplete.
-            autocorrect: true,
-            //Keyboard type is set to email.
-            keyboardType: TextInputType.emailAddress,
-            decoration: const InputDecoration(
-              hintText: "Email adresinizi giriniz.",
-              //Adding an icon to the head.
-              prefixIcon: Icon(Icons.mail),
-            ),
-          ),
-          const SizedBox(
-            height: 40.0,
-          ),
-          TextFormField(
-            //To hide the password.
-            obscureText: true,
-            decoration: const InputDecoration(
-              hintText: "Şifrenizi giriniz.",
-              //Adding an icon to the head.
-              prefixIcon: Icon(Icons.lock),
-            ),
-          ),
-          const SizedBox(
-            height: 40.0,
-          ),
-          Row(
-            children: [
-              Expanded(
-                child: TextButton(
-                  onPressed: () {},
-                  style: TextButton.styleFrom(
-                    primary: Colors.white,
-                    backgroundColor: Colors.blue,
-                  ),
-                  child: const Text(
-                    "Hesap Oluştur",
-                    style: TextStyle(
-                      fontSize: 20.0,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-              ),
-              const SizedBox(
-                width: 20.0,
-              ),
-              Expanded(
-                child: TextButton(
-                  onPressed: () {},
-                  style: TextButton.styleFrom(
-                    primary: Colors.white,
-                    backgroundColor: Colors.blue,
-                  ),
-                  child: const Text(
-                    "Giriş Yap",
-                    style: TextStyle(
-                      fontSize: 20.0,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ],
       ),
     );
+  }
+
+  void _login() {
+    if (_formKey.currentState!.validate()) {
+      print("Giriş işlemlerini başlat");
+    }
   }
 }
