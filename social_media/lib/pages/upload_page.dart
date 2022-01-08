f6bc34d7a3f8ccdeade156ade7cb334df9af459c
alihan-ulker <ali.ulker.217@gmail.com>
2022-01-09 00:47:04 +0300

    Upload page updated

--- social_media/lib/pages/upload_page.dart
+++ social_media/lib/pages/upload_page.dart
@@ -1,15 +1,34 @@
+//@dart=2.9
+import 'dart:io';
 import 'package:flutter/material.dart';
 
 class UploadPage extends StatefulWidget {
-  const UploadPage({Key? key}) : super(key: key);
+  const UploadPage({Key key}) : super(key: key);
 
   @override
   _UploadPageState createState() => _UploadPageState();
 }
 
 class _UploadPageState extends State<UploadPage> {
+  File file;
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("Yükle Sayfası"));
+    return file == null ? uploadButton() : postForm();
+  }
+
+  Widget uploadButton() {
+    return IconButton(
+        onPressed: () {},
+        icon: const Icon(
+          Icons.file_upload,
+          size: 50.0,
+          //color: Colors.black,
+        ));
+  }
+
+  Widget postForm() {
+    return const Center(
+      child: Text("Yüklenen resim ve text alanları gelecek."),
+    );
   }
 }
