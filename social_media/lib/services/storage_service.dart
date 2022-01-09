1b07aea3cb1cd0e923e94bf91eb54533d40f852f
alihan-ulker <ali.ulker.217@gmail.com>
2022-01-10 00:26:13 +0300

    Uuid added
    uuid: package added.
    A random and unique name is assigned to each uploaded image.

--- social_media/lib/services/storage_service.dart
+++ social_media/lib/services/storage_service.dart
@@ -1,15 +1,18 @@
 import 'dart:io';
 
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:uuid/uuid.dart';
 
 //Firebase's Storage service was used to upload posts and images.
 class StorageService {
   Reference _storage = FirebaseStorage.instance.ref();
+  late String photoId;
 
 //The image requested to be loaded has been sent to the putFile method.
   Future<String> postImageUpload(File imageFile) async {
+    photoId = Uuid().v4();
     UploadTask uploadManager =
-        _storage.child("images/posts/post.jpg").putFile(imageFile);
+        _storage.child("images/posts/post_$photoId.jpg").putFile(imageFile);
     TaskSnapshot snapshot = await uploadManager;
     String uploadedPhotoUrl = await snapshot.ref.getDownloadURL();
     return uploadedPhotoUrl;
